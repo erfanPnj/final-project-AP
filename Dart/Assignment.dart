061cad09579a11bb76834a5ca65227eb41262516
erfanPnj <erfanpanjehshahi@gmail.com>
2024-07-10 19:18:36 +0330

    Revert "Merge branch 'flutter'"
    This reverts commit 173ea6ebb087868cb3644d202704dd3a53dd0ab7, reversing
    changes made to 081f61c84f80d290642160e849d101af8760b4b4.

--- Dart/Assignment.dart
+++
@@ -1,9 +0,0 @@
-class Assignment {
-  String name;
-  int deadline;
-  bool status;
-  String courseId;
-  String definingDate;
-
-  Assignment(this.name, this.deadline, this.status, this.courseId, this.definingDate);
-}
